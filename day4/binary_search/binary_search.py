364beaf757c880fd9f6816f0d08f7202b385756f
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-21 01:46:08 +0300

    Updates Binary Search to use only the search_item to determine it's percentile. The old implementation was cheating this process.

--- day4/binary_search/binary_search.py
+++ day4/binary_search/binary_search.py
@@ -13,46 +13,42 @@ class BinarySearch:
             value += self.b
         return result
 
-    def search(self, item):
-        try:
-            search_item_index = self.created_list.index(item)
-        except ValueError:
-            search_item_index = -1
+    def search(self, search_item):
+        target_list = self._get_target_list(search_item)
+        return self._binary_search(search_item, target_list, 0)
 
-        target_list = self._get_target_portion(search_item_index)
-        return self._binary_search(item, search_item_index, target_list, 0)
-
-    def _get_target_portion(self, index):
+    def _get_target_list(self, search_item):
         # Split the created_list into 5 portions and return the one that has the item
         percentiles = 5
         percentile_step = len(self.created_list) / percentiles
-        percentile_1, percentile_2 = percentile_step, percentile_step*2
-        percentile_3, percentile_4 = percentile_step*3, percentile_step*4
+        percentile_limit_1, percentile_limit_2 = percentile_step, percentile_step * 2
+        percentile_limit_3, percentile_limit_4 = percentile_step * 3, percentile_step * 4
 
-        item_percentile = int((float(index)/float(len(self.created_list))) * percentiles) * len(self.created_list)
-        if item_percentile <= percentile_1:
-            return self.created_list[:percentile_1]
-        if item_percentile <= percentile_2:
-            return self.created_list[percentile_1:percentile_2]
-        if item_percentile <= percentile_3:
-            return self.created_list[percentile_2:percentile_3]
-        if item_percentile <= percentile_4:
-            return self.created_list[percentile_3:percentile_4]
-        if item_percentile > percentile_4:
-            return self.created_list[percentile_4:]
+        last_element = self.created_list[len(self.created_list) - 1]
+        item_percentile = float(search_item) / float(last_element) * percentiles
+        if item_percentile <= 1:
+            return self.created_list[:percentile_limit_1]
+        if 1 < item_percentile <= 2:
+            return self.created_list[percentile_limit_1:percentile_limit_2]
+        if 2 < item_percentile <= 3:
+            return self.created_list[percentile_limit_2:percentile_limit_3]
+        if 3 < item_percentile <= 4:
+            return self.created_list[percentile_limit_3:percentile_limit_4]
+        if item_percentile > 4:
+            return self.created_list[percentile_limit_4:]
 
-    def _binary_search(self, search_item, search_item_index, items, loops):
+    def _binary_search(self, search_item, items, loops):
         if search_item == items[len(items) - 1]:
             loops = loops - 1 if not loops == 0 else loops
-            return {'count': loops, 'index': search_item_index}
+            return {'count': loops, 'index': self.created_list.index(search_item)}
         elif len(items) == 2 and search_item not in items:
-            return {'count': loops - 1, 'index': search_item_index}
+            return {'count': loops - 1, 'index': -1}
 
         middle = len(items) / 2
         first_half, last_half = items[:middle], items[middle:]
         valid_half = first_half if search_item <= first_half[len(first_half) - 1] else last_half
 
-        return self._binary_search(search_item, search_item_index, valid_half, loops + 1)
+        return self._binary_search(search_item, valid_half, loops + 1)
 
     def __getitem__(self, index):
         return self.created_list[index]
