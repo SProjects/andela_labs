36d5d006a59bdf5dfeb310b5ce1151e4113a189c
Sebuuma Daniel <sedzsoft@gmail.com>
2017-04-21 10:25:18 +0300

    Renames variable to make more descriptive.

--- day4/binary_search/binary_search.py
+++ day4/binary_search/binary_search.py
@@ -1,16 +1,16 @@
 class BinarySearch:
     def __init__(self, a, b):
-        self.a = a
-        self.b = b
-        self.created_list = self._generate_list()
-        self.length = len(self.created_list)
+        self.list_size = a
+        self.step = b
+        self.generated_list = self._generate_list()
+        self.length = len(self.generated_list)
 
     def _generate_list(self):
         result = []
-        value = self.b
-        while len(result) < self.a:
+        value = self.step
+        while len(result) < self.list_size:
             result.append(value)
-            value += self.b
+            value += self.step
         return result
 
     def search(self, search_item):
@@ -19,35 +19,35 @@ class BinarySearch:
 
     def _get_target_list(self, search_item):
         """
-        This method divides the self.created_list into 5 portions/percentiles. Then uses the search_item to calculate
+        This method divides the self.generated_list into 5 portions/percentiles. Then uses the search_item to calculate
          the portion/percentile the it belongs to. The calculated portion/percentile is then used to determine which
-         slice of self.created_list to return for search.
+         slice of self.generated_list to return for search.
 
         :param search_item:
-        :return: slice of self.created_list that contains the search_item
+        :return: slice of self.generated_list that contains the search_item
         """
         percentiles = 5
-        percentile_step = len(self.created_list) / percentiles
+        percentile_step = len(self.generated_list) / percentiles
         percentile_limit_1, percentile_limit_2 = percentile_step, percentile_step * 2
         percentile_limit_3, percentile_limit_4 = percentile_step * 3, percentile_step * 4
 
-        last_element = self.created_list[len(self.created_list) - 1]
-        item_percentile = float(search_item) / float(last_element) * percentiles
-        if item_percentile <= 1:
-            return self.created_list[:percentile_limit_1]
-        if 1 < item_percentile <= 2:
-            return self.created_list[percentile_limit_1:percentile_limit_2]
-        if 2 < item_percentile <= 3:
-            return self.created_list[percentile_limit_2:percentile_limit_3]
-        if 3 < item_percentile <= 4:
-            return self.created_list[percentile_limit_3:percentile_limit_4]
-        if item_percentile > 4:
-            return self.created_list[percentile_limit_4:]
+        last_element = self.generated_list[len(self.generated_list) - 1]
+        search_item_percentile = float(search_item) / float(last_element) * percentiles
+        if search_item_percentile <= 1:
+            return self.generated_list[:percentile_limit_1]
+        if 1 < search_item_percentile <= 2:
+            return self.generated_list[percentile_limit_1:percentile_limit_2]
+        if 2 < search_item_percentile <= 3:
+            return self.generated_list[percentile_limit_2:percentile_limit_3]
+        if 3 < search_item_percentile <= 4:
+            return self.generated_list[percentile_limit_3:percentile_limit_4]
+        if search_item_percentile > 4:
+            return self.generated_list[percentile_limit_4:]
 
     def _binary_search(self, search_item, items, loops):
         if search_item == items[len(items) - 1]:
             loops = loops - 1 if not loops == 0 else loops
-            return {'count': loops, 'index': self.created_list.index(search_item)}
+            return {'count': loops, 'index': self.generated_list.index(search_item)}
         elif len(items) == 2 and search_item not in items:
             return {'count': loops - 1, 'index': -1}
 
@@ -58,4 +58,4 @@ class BinarySearch:
         return self._binary_search(search_item, valid_half, loops + 1)
 
     def __getitem__(self, index):
-        return self.created_list[index]
+        return self.generated_list[index]
